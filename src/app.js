136cdba6ebb21e8e4cb815e195b8f409c5d297e4
Manish Jung <eternalmahesh007@gmail.com>
2021-06-08 18:33:38 +0545

    first commmit

--- src/app.js
+++ src/app.js
@@ -19,7 +19,37 @@ app.post("/students", (req,res)=> {
         })
 })
 
+// get all data
+
+app.get("/students", async (req,res) =>{
+    try{
+       const studentsData = await Student.find();
+       res.send(studentsData);
+    }catch(e){
+        res.send(e);
+
+    }
+
+})
+
+//Get indivisual data if asked not all
+app.get("/students/:id", async(req,res)=> {
+    try{
+        const _id = req.params.id;
+        const studentData = await Student.findById(_id);
+         res.send(studentData);
+       
+
+    }catch(e){
+        res.send(e);
+
+    }
+
+})
+
+
 app.listen(port, ()=>{
     console.log(`Node.js server is running on port ${port}`);
 });
+
 
