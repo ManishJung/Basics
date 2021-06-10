4fa5c276478a5cfd77de7d2cb3ff62855f63ce01
Manish Jung <eternalmahesh007@gmail.com>
2021-06-10 11:12:25 +0545

    second commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/students_api",{
+mongoose.connect(process.env.DB_CONNECT,{
     useCreateIndex: true, useNewUrlParser:true, useUnifiedTopology:true
      }).then(()=>{
          console.log("Connected to DB");
